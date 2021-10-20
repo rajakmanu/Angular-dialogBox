9515b630a9c80973d3f2de2f7cfc3c34d791ac8b
Manisha Rajak <manisha.rajak@biascorp.com>
2021-10-20 13:27:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { MatDialog,MatDialogConfig} from '@angular/material/dialog';
+import { NewSkillComponent } from './new-skill/new-skill.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dialog-example';
+  constructor(public dialog:MatDialog){}
+  addSkill(){
+    const dialogConfig=new MatDialogConfig();
+    dialogConfig.width="60%";
+    dialogConfig.height="60%"
+    dialogConfig.autoFocus=true;
+    dialogConfig.disableClose=false;
+    this.dialog.open(NewSkillComponent,dialogConfig);  //open method takes 2 parameters->component and optional configuration
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatDialogModule} from "@angular/material/dialog";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewSkillComponent } from './new-skill/new-skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewSkillComponent
   ],
+  entryComponents:[NewSkillComponent], 
+   //a  component used for a dialog has to be added in the entrycomponent array
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
